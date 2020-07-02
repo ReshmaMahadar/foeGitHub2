11fba458f1ce800de4e7d971ff5c3c6e14bee26f
USER <USER@HP>
2020-07-03 01:23:33 +0530

    2nd push, added 2nd SOP

--- src/com/cjc/GitDemo2.java
+++ src/com/cjc/GitDemo2.java
@@ -5,6 +5,7 @@ public class GitDemo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("inside git2");
+		System.out.println("add this stmnt in 2nd attempt");
 
 	}
 
